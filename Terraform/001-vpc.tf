73de7f692e565f65d3fa2f81b5e12b219e887d51
Ibe Chuks <ibechuksvictor@gmail.com>
2023-07-07 10:03:07 +0100

    infrastructure vpc

---
+++ Terraform/001-vpc.tf
@@ -0,0 +1,34 @@
+#----------------------------------------------------
+#                       VPC
+#----------------------------------------------------
+
+data "aws_availability_zones" "azs" {}
+
+module "capstone-24-EKS-vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "3.19.0"
+
+  name            = "${var.project_name}-vpc"
+  cidr            = var.vpc_cidr_block
+  private_subnets = var.private_subnet_cidr_blocks
+  public_subnets  = var.public_subnet_cidr_blocks
+  azs             = data.aws_availability_zones.azs.names
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"           = 1
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${var.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                    = 1
+  }
+}
